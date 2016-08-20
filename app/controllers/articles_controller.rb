f75f6329ec6b8eb17f983366ca1b1280998ef8b8
Trevor Lawrence <tlawren3@icloud.com>
2016-08-20 13:22:32 -0700

    moved controller functionality to reduce redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -9,7 +11,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
@@ -26,7 +28,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
+
 
     if @article.update(article_params)
 
@@ -39,18 +41,22 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    set_article
     @article.destroy
 
     flash[:notice] = "Article deleted"
     redirect_to articles_path
   end
 
-  private def article_params
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  def article_params
     params.require(:article).permit(:title, :description)
   end
 end
