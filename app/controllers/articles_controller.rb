0b4fa7a2c73689246abd7f4029408baa4a369eba
Trevor Lawrence <tlawren3@icloud.com>
2016-08-20 13:14:59 -0700

    moved functionality to partials. Destroy function implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,11 +36,18 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
-
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash[:notice] = "Article deleted"
+    redirect_to articles_path
   end
 
   private def article_params
